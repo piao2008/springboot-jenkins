2f1f92aef7006f9c179b1c6c0db9c382a22849da
piao2080 <76135481@qq.com>
2025-02-16 00:04:55 +0800

    添加 配置prometheus信息和添加依赖

--- src/main/java/com/as/springbootjenkins/SpringbootJenkinsApplication.java
+++ src/main/java/com/as/springbootjenkins/SpringbootJenkinsApplication.java
@@ -1,7 +1,10 @@
 package com.as.springbootjenkins;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootJenkinsApplication {
@@ -9,5 +12,9 @@ public class SpringbootJenkinsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJenkinsApplication.class, args);
     }
-
+    // 可选：注册一个 TimedAspect 来自动记录方法执行时间等指标。
+    @Bean
+    public TimedAspect timedAspect(MeterRegistry registry) {
+        return new TimedAspect(registry);
+    }
 }
